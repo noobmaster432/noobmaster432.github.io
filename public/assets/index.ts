1e1c2f2bf483855f82c95793cd3db1f49d4f9e67
Gyanendra Tiwari <mrgyan432@gmail.com>
2024-07-11 20:47:24 +0530

    initial commit

--- public/assets/index.ts
+++
@@ -1,8 +0,0 @@
-import buzznet from './buzznet.png';
-import filmfiesta from './filmfiesta.png';
-import nivaas from './nivaas.png';
-import Gyan from './Gyan.png';
-import photo from './photo.png';
-import cody from './cody.png';
-
-export { buzznet, filmfiesta, nivaas, Gyan, photo, cody };
